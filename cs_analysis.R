77168634138c786a5b52be90109acb0fe8a7c626
michbur <michalburdukiewicz@gmail.com>
2015-04-30 16:36:39 +0200

    fixed some problems in code, ready for analysis

--- cs_analysis.R
+++ cs_analysis.R
@@ -22,13 +22,14 @@ cs_data <- t(sapply(seqs_pos, function(i) {
 
 
 #roi - region of the interest
-ngrams_freqs <- function(roi, background, n = 1) {
+#important - use full d argument, ngrams_freqs doesn't recycle d as other function do
+ngrams_freqs <- function(roi, background, n = 1, d = 0) {
   
   ##
   # calculate background -----------------------------------
   ##
   
-  bg_table <- background %>% as.matrix %>% seq2ngrams(n, a()[-1], pos = FALSE) %>%
+  bg_table <- background %>% as.matrix %>% seq2ngrams(n, a()[-1], d, pos = FALSE) %>%
     unlist %>% table %>% data.frame 
   
   bg_counts <- bg_table[["Freq"]]
@@ -39,7 +40,7 @@ ngrams_freqs <- function(roi, background, n = 1) {
   # calculate amino acids frequency in cs -----------------------------------
   ##
   
-  roi_ngrams <- roi %>% as.matrix %>% seq2ngrams(n, a()[-1], pos = FALSE)
+  roi_ngrams <- roi %>% as.matrix %>% seq2ngrams(n, a()[-1], d, pos = FALSE)
   
   roi_counts <- cbind(bg_table[["."]], do.call(cbind, lapply(1L:ncol(roi_ngrams), function(single_position) {
     factor(roi_ngrams[, single_position], levels = bg_table[["."]]) %>% 
@@ -47,7 +48,7 @@ ngrams_freqs <- function(roi, background, n = 1) {
   })))
   #solve it later by smart select call with .dots
   
-  colnames(roi_counts) <- c("ngrams", sapply(as.character(c(-5:(5 - n + 1))), function(i) 
+  colnames(roi_counts) <- c("ngrams", sapply(as.character(c(-5:(5 - n - sum(d) + 1))), function(i) 
     ifelse(substr(i, 0, 1) == "-", i, paste0("+", i))))
     
   mroi_counts <- melt(roi_counts)
@@ -56,11 +57,14 @@ ngrams_freqs <- function(roi, background, n = 1) {
   mroi_counts
 }
 
-freqs2 <- ngrams_freqs(cs_data, bg_data, 1)
+freqs2 <- ngrams_freqs(cs_data, bg_data, 3, d = c(1, 0))
 
 library(entropy)
 #calculate entropy to assess which sites ae the most informative
-group_by(freqs2, variable) %>% summarise(entr = entropy.empirical(value))
+group_by(freqs2, variable) %>% summarise(entropy = entropy.empirical(value))
+
+group_by(freqs2, variable) %>% filter(freq > quantile(freq, 0.99))
+
 
 ggplot(freqs2, aes(x = variable, y = value, fill = ngrams)) +
   geom_bar(stat = "identity", position = "dodge") +
