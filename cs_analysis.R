2fcb2624da5e4d33b5c0cb196160ae451176525b
michbur <michalburdukiewicz@gmail.com>
2015-04-30 12:43:03 +0200

    cleavage site analysis raw script

--- cs_analysis.R
+++ cs_analysis.R
@@ -3,42 +3,35 @@ source("start.R")
 #to predict cleavage sites, I need to analyze their properties.
 
 #sequences with cleavage sites
-pos_seqs <- read_uniprot(paste0(pathway, "sept_signal.txt"), euk = TRUE)
+seqs_pos <- read_uniprot(paste0(pathway, "sept_signal.txt"), euk = TRUE)
 
-
-#########################
-# calculate background -----------------------------------
-#########################
-
-background <- data.frame(t(sapply(pos_seqs, function(i) {
+#aminoacids in background
+bg_data <- data.frame(t(sapply(seqs_pos, function(i) {
   i[2L:51]
-}))) %>% unlist %>% table %>% data.frame %>% select(Freq) %>% unlist
+})))
 
-
-
-
-#########################
-# calculate amino acids frequency in cs -----------------------------------
-#########################
-
-#index number of the cleavage site
-id_cs <- sapply(pos_seqs, function(i) attr(i, "sig")[2])
-
-
-#conensus after Heijne 1983 and Hiller 2004
-
-consensus <- t(sapply(pos_seqs, function(i) {
+#cleavage site data
+cs_data <- t(sapply(seqs_pos, function(i) {
   #cs is the posion of the FIRST amino acid after cleavage site
   cs <- attr(i, "sig")[2]
   #positions: -3, -1, +1
   i[(cs - 6):(cs+4)]
 }))
 
-consensus <- data.frame(consensus)
 
-mconsensus <- melt(data.frame(a = rownames(apply(consensus, 2, table)), apply(consensus, 2, table)))
+##
+# calculate background -----------------------------------
+##
 
-levels(mconsensus[["variable"]]) <- sapply(as.character(c(-5:5)), function(i) 
+bg_counts <- bg_data %>% unlist %>% table %>% data.frame %>% select(Freq) %>% unlist
+
+##
+# calculate amino acids frequency in cs -----------------------------------
+##
+
+mcs_counts <- melt(data.frame(a = rownames(apply(cs_data, 2, table)), apply(cs_data, 2, table)))
+
+levels(mcs_counts[["variable"]]) <- sapply(as.character(c(-5:5)), function(i) 
   ifelse(substr(i, 0, 1) == "-", i, paste0("+", i)))
 
 #posible way how to reorder amino acids to make them appear in the order as they are in our aaagregation
@@ -50,13 +43,13 @@ aa_groups <- list(`1` = c("k", "r", "h"),
                   `4` = c("d", "e", "a", "p", "y", "g"))
 
 #add group information to amino acids
-suppressWarnings(mconsensus <- cbind(group = sapply(names(unlist(aa_groups)), substr, 0, 1), mconsensus))
+suppressWarnings(mcs_counts <- cbind(group = sapply(names(unlist(aa_groups)), substr, 0, 1), mcs_counts))
 #suppress expected warning 'row names were found from a short variable and have been discarded'
 
-mconsensus[["value"]] <- mconsensus[["value"]]/background
+mcs_counts[["value"]] <- mcs_counts[["value"]]/bg_counts
 
 
-ggplot(mconsensus, aes(x = variable, y = value, fill = a)) +
+ggplot(mcs_counts, aes(x = variable, y = value, fill = a)) +
   geom_bar(stat = "identity", position = "dodge") +
   scale_x_discrete("Position") +
   scale_y_continuous("Frequency") +
