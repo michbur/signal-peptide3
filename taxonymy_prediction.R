5f22077c84b5ac4cb3dc73813b667f03f69f5e03
michbur <michalburdukiewicz@gmail.com>
2015-05-24 21:12:47 +0200

    hopefully finished all_oc_seqs.csv

--- taxonymy_prediction.R
+++ taxonymy_prediction.R
@@ -11,24 +11,26 @@ if(Sys.info()["nodename"] == "phobos" )
 
 file_names <- c(paste0("nonsignal_peptides", 1L:9), "signal_peptides")
 
-all_oc <- unique(unlist(pblapply(file_names, function(single_file) {
-  load(paste0(pathway, single_file, ".RData"))
-  #just to remove Eukaryota from each oc
-  oc <- unique(unlist(sapply(strsplit(sapply(seqs, function(i) attr(i, "OC")), "; "), 
-                             function(i) i[-1])))
-  sub(".", "", oc, fixed = TRUE)
-})))
+# all_oc <- unique(unlist(pblapply(file_names, function(single_file) {
+#   load(paste0(pathway, single_file, ".RData"))
+#   #just to remove Eukaryota from each oc
+#   oc <- unique(unlist(sapply(strsplit(sapply(seqs, function(i) attr(i, "OC")), "; "), 
+#                              function(i) i[-1])))
+#   sub(".", "", oc, fixed = TRUE)
+# })))
+# 
+# save(all_oc, file = paste0(pathway, "all_oc.RData"))
 
-save(all_oc, file = paste0(pathway, "all_oc.RData"))
+load(paste0(pathway, "all_oc.RData"))
 
-all_oc_seqs <- pblapply(file_names, function(single_file) {
+all_oc_seqs <- do.call(cbind, pblapply(file_names, function(single_file) {
   load(paste0(pathway, single_file, ".RData"))
-  as.simple_triplet_matrix(vapply(seqs[1L:10], function(single_seq) {
+  as.simple_triplet_matrix(vapply(seqs, function(single_seq) {
     oc <- strsplit(attr(single_seq, "OC"), "; ")[[1]][-1]
     oc[length(oc)] <- sub(".", "", oc[length(oc)], fixed = TRUE)
     #c(attr(single_seq, "OS"))
     all_oc %in% oc
   }, rep(TRUE, length(all_oc))))
-})
+}))
 
-write.csv2(all_oc_seqs, file = paste0(pathway, "all_oc_seqs.csv"))
+write.csv2(as.matrix(all_oc_seqs), file = paste0(pathway, "all_oc_seqs.csv"))
