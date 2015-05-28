1ef01dd5f905716e50a8188167b33f6ab336d203
michbur <michalburdukiewicz@gmail.com>
2015-05-28 18:14:54 +0200

    trying to debug whole taxonomy predictions

--- whole_taxonomy_prediction.R
+++ whole_taxonomy_prediction.R
@@ -18,22 +18,25 @@ unique_ocs <- unique(unlist(strsplit(as.character(oc_pred[["oc"]]), "; ")))[-1]
 full_oc_metrics <- pblapply(unique_ocs, function(single_oc) {
   dat <- oc_pred[grepl(single_oc, oc_pred[["oc"]]), c("signalHsmm", "signalP", "real")]
   both <- length(unique(dat[, "real"])) == 2
-  if(both) {
+  list(metrics = if(both) {
     HMeasure(dat[["real"]], dat[, c("signalHsmm", "signalP")])[["metrics"]]
   } else {
     NULL
-  }
+  }, count = nrow(dat),
+  what = single_oc)
 })
 
-oc_counts <- sapply(unique_ocs, function(single_oc)
-  length(grep(single_oc, oc_pred[["oc"]])))[!sapply(full_oc_metrics, is.null)]
-  
-#proper oc metrics
-oc_pmetrics <- full_oc_metrics[!sapply(full_oc_metrics, is.null)]
+save(full_oc_metrics, unique_ocs, oc_counts, file = "/home/michal/Dropbox/signal-peptide2_data/whole_taxonomy_snap.RData")
 
-oc_AUC <- data.frame(names(oc_counts), oc_counts, t(sapply(oc_pmetrics, function(i) i[, "AUC"])))
-colnames(oc_AUC) <- c("OC", "Count", "signalHsmm", "signalP")
-oc_AUC <- cbind(oc_AUC, diff = oc_AUC[, "signalHsmm"] - oc_AUC[, "signalP"])
-oc_AUC <- oc_AUC[order(oc_AUC[, "diff"], decreasing = TRUE), ]
-write.csv2(oc_AUC[oc_AUC[, "Count"] > 50, ], file = paste0(pathway, "oc_AUC50.csv"))
-write.csv2(oc_AUC, file = paste0(pathway, "oc_AUC.csv"))
+oc_counts <- pbsapply(unique_ocs, function(single_oc)
+  sum(grepl(single_oc, oc_pred[["oc"]])))
+  
+# #proper oc metrics
+# oc_pmetrics <- full_oc_metrics[!sapply(full_oc_metrics, is.null)]
+# 
+# oc_AUC <- data.frame(names(oc_counts), oc_counts, t(sapply(oc_pmetrics, function(i) i[, "AUC"])))
+# colnames(oc_AUC) <- c("OC", "Count", "signalHsmm", "signalP")
+# oc_AUC <- cbind(oc_AUC, diff = oc_AUC[, "signalHsmm"] - oc_AUC[, "signalP"])
+# oc_AUC <- oc_AUC[order(oc_AUC[, "diff"], decreasing = TRUE), ]
+# write.csv2(oc_AUC[oc_AUC[, "Count"] > 50, ], file = paste0(pathway, "oc_AUC50.csv"))
+# write.csv2(oc_AUC, file = paste0(pathway, "oc_AUC.csv"))
