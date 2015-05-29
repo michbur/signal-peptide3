fb53c47bd771f998fdb7e711185ecaaf196c1927
michbur <michalburdukiewicz@gmail.com>
2015-05-29 10:39:34 +0200

    added some plots

--- whole_taxonomy_prediction.R
+++ whole_taxonomy_prediction.R
@@ -15,6 +15,8 @@ oc_pred <- read.csv2(file = paste0(pathway, "osoc_pred.csv"))
 #-1 to remove Eukaryota
 unique_ocs <- unique(unlist(strsplit(as.character(oc_pred[["oc"]]), "; ")))[-1]
 
+unique_ocs <- sub(".", "", unique_ocs, fixed = TRUE)
+
 full_oc_metrics <- pblapply(unique_ocs, function(single_oc) {
   dat <- oc_pred[grepl(single_oc, oc_pred[["oc"]]), c("signalHsmm", "signalP", "real")]
   both <- length(unique(dat[, "real"])) == 2
@@ -39,6 +41,12 @@ oc_AUC <- data.frame(unique_ocs[informative], t(sapply(full_oc_metrics[informati
 colnames(oc_AUC) <- c("OC", "Count", "signalHsmm", "signalP")
 oc_AUC <- cbind(oc_AUC, diff = oc_AUC[, "signalHsmm"] - oc_AUC[, "signalP"])
 oc_AUC <- oc_AUC[order(oc_AUC[, "diff"], decreasing = TRUE), ]
+
+#no necessary now, I'm filtering unique_ocs
+#levels(oc_AUC[["OC"]]) <- sub(".", "", levels(oc_AUC[["OC"]]), fixed = TRUE)
+#oc_AUC <- oc_AUC[!duplicated(oc_AUC), ]
+
+
 write.csv2(oc_AUC[oc_AUC[, "Count"] > 50, ], file = paste0(pathway, "oc_AUC50.csv"))
 write.csv2(oc_AUC, file = paste0(pathway, "oc_AUC.csv"))
 
